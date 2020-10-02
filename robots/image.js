aaa8581c74405690e235cc9ab2343518f1d4c321
towcorp <towcorp@hotmail.com>
2020-10-02 17:15:09 -0300

    feat: add image download logic with fallback

--- robots/image.js
+++ robots/image.js
@@ -8,10 +8,10 @@ const googleSearchCredentials = require('../credentials/google-search.json')
 async function robot() {
 	const content = state.load()
 	
-	//await fetchImagesOfAllSentences(content)
+	await fetchImagesOfAllSentences(content)
 	await downloadAllImages(content)
 	
-	//state.save(content)
+	state.save(content)
 	
 	async function fetchImagesOfAllSentences(content) {
 		for (const sentence of content.sentences) {
