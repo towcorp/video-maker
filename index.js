d72b4b5b922bebdb96cf30bc4727c1003ceba643
towcorp <towcorp@hotmail.com>
2020-10-03 09:41:08 -0300

    feat: create video robot

--- index.js
+++ index.js
@@ -1,37 +1,17 @@
-const readline = require('readline-sync')
 const robots = {
-	//userInput: require('./robots/user-Input')
-    text: require('./robots/text.js')
+	input: require('./robots/input.js'),
+    text: require('./robots/text.js'),
+	state: require('./robots/state.js'),
+	image: require('./robots/image.js')
 }
 
 async function start() {
-	const content = {}
-
-	content.searchTerm = askAndReturnSearchTerm()
-	content.prefix = askAndReturnPrefix()
+	robots.input()
+	await robots.text()
+	await robots.image()
 	
-	//robots.userInput(content)
-	await robots.text(content)
-	
-
-	function askAndReturnSearchTerm() {
-		return readline.question('Type a Wikipedia search term: ')
-	}
-
-	function askAndReturnPrefix() {
-		const prefixes = ['Who is', 'What is', 'The history of']
-		const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option: ')
-		const selectedPrefixText = prefixes[selectedPrefixIndex]
-		
-
-		return selectedPrefixText
-<<<<<<< HEAD
-	}
-=======
-    }
-
->>>>>>> aa1a8da2b52d3f0e119a044cdd82c3dd84eb7296
-	console.log(content)
+	const content = robots.state.load()
+	console.dir(content, { depth: null })
 }
 
 start()
